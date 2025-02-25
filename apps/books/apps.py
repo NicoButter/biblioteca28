5491bdf9d0ca6f10af8c7a11cfeb1b8122338106
Nicolás Butterfield <nicobutter@gmail.com>
2025-02-25 11:01:05 -0300

    second deploy

--- apps/books/apps.py
+++ apps/books/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NewspaperConfig(AppConfig):
+class BooksConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "newspaper"
+    name = "apps.books"
--- biblioteca28/settings.py
+++ biblioteca28/settings.py
@@ -9,11 +9,16 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import sys
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps.accounts",
+    "app.books",
+    "app.newspaper"
 ]
 
 MIDDLEWARE = [

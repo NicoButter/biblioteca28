8a752c1cc804db8c0eb010c30635986e6658a0fe
Nicolás Butterfield <nicobutter@gmail.com>
2025-02-26 10:39:42 -0300

    login updates

--- biblioteca28/settings.py
+++ biblioteca28/settings.py
@@ -129,7 +129,13 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'accounts.Usuario'
 
-STATIC_URL = "static/"
+# Directorio para archivos estáticos
+STATIC_URL = '/static/'
+
+# Directorios donde Django buscará archivos estáticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'apps/accounts/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

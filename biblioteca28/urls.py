ed39f257d0a4686c4109f1dfc7a84137ca713d69
Nicolás Butterfield <nicobutter@gmail.com>
2025-02-27 11:20:34 -0300

    dashboar and base updates

--- biblioteca28/urls.py
+++ biblioteca28/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from apps.accounts.views import custom_login
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),

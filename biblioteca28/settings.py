a0afb4348f629efac2985700f38b40eaa6d849b0
Nicolás Butterfield <nicobutter@gmail.com>
2025-03-06 09:44:47 -0300

    updates on footer styles

--- biblioteca28/settings.py
+++ biblioteca28/settings.py
@@ -136,6 +136,7 @@ STATIC_URL = '/static/'
 
 # Directorios donde Django buscará archivos estáticos
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'apps/accounts/static'),
 ]
 

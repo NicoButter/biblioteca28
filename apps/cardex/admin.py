cd00d3c06a222245b00b3417708090bcb1aec40b
Nicolás Butterfield <nicobutter@gmail.com>
2025-03-10 10:35:49 -0300

    books admin update

--- apps/cardex/admin.py
+++ apps/cardex/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cardex
 
-# Register your models here.
+@admin.register(Cardex)
+class CardexAdmin(admin.ModelAdmin):
+    list_display = ('newspaper', 'issue_number', 'circulation', 'created_at')
+    search_fields = ('newspaper__title', 'issue_number')
+    list_filter = ('created_at',)
